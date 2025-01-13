3febfe6d99adfd5625c13b35e2b7dfe8696d7235
RINTF <rittmann@integration-factory.de>
2025-01-13 11:14:54 +0100

    Update streamlit_app.py
    no stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 df = my_dataframe.to_pandas()
 st.dataframe(df)
 #st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
 name_on_order = st.text_input("Name on Smoothie:")
