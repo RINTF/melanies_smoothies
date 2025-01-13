3181ba8930ae628d331521ba52dba6777dbcb835
RINTF <rittmann@integration-factory.de>
2025-01-13 10:37:34 +0100

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json, use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
     ingredients_string = ''
