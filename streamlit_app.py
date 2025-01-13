e82e8306e81c119f038a8f1d033dd68f8946ae23
RINTF <rittmann@integration-factory.de>
2025-01-13 10:06:33 +0100

    Update streamlit_app.py
    Move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ st.write(
     """
 )
 
-session = get_active_session()
+# session = get_active_session()
+cnx = st.connect("streamlit")
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
